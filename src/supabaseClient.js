250cb8d02714df7d83c1f0fcf1447dab4b3e8756
Choiseonkyeong <94091135+Choiseonkyeong@users.noreply.github.com>
2026-08-27 16:23:40 +0900

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,9 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Choiseonkyeong's Project 대시보드에서 복사한 URL 및 Anon Key
-const supabaseUrl = 'https://seuamhutmdcjyacxuqyt.supabase.co';
+const supabaseUrl = 'https://lanllylumkvkowcnuawt.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhbmlseWl1bWt2a293Y251YXd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc3OTg5MjIsImV4cCI6MjEwMzM3NDkyMn0.1AZFoSC4_LHUkTo2flmGN7vPYB8MgEICaDohbn3fPg0';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
-
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    persistSession: false,
+    autoRefreshToken: false,
+    detectSessionInUrl: false
+  }
+});
