ee7832e2b7da1f017aed189e1dc7f6312d235b3e
Choiseonkyeong <94091135+Choiseonkyeong@users.noreply.github.com>
2026-08-27 14:53:54 +0900

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://lanllylumkvkowcnuawt.supabase.co';
 // Legacy 탭에서 복사한 eyJ... 로 시작하는 anon key
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'; 
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhbmlseWl1bWt2a293Y251YXd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc3OTg5MjIsImV4cCI6MjEwMzM3NDkyMn0.1AZFoSC4_LHUkTo2flmGN7vPYB8MgEICaDohbn3fPg0'; 
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
