16675cf577ff112e1e4814cb54084662958f9c6f
Choiseonkyeong <94091135+Choiseonkyeong@users.noreply.github.com>
2026-08-27 16:06:37 +0900

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -4,6 +4,5 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = 'https://seuamhutmdcjyacxuqyt.supabase.co';
 
 // 해당 프로젝트 대시보드(Project Settings > API)에서 복사한 anon public key를 넣어주세요.
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhbmlseWl1bWt2a293Y251YXd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc3OTg5MjIsImV4cCI6MjEwMzM3NDkyMn0.1AZFoSC4_LHUkTo2flmGN7vPYB8MgEICaDohbn3fPg0'; 
-
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhbmlseWl1bWt2a293Y251YXd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc3OTg5MjIsImV4cCI6MjEwMzM3NDkyMn0.1AZFoSC4_LHUkTo2flmGN7vPYB8MgEICaDohbn3fPg0';
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
