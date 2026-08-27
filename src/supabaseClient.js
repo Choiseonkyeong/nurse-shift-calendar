f9ce1199396316e6329bf4a66ea5be207e5da7c0
Choiseonkyeong <94091135+Choiseonkyeong@users.noreply.github.com>
2026-08-27 13:18:25 +0900

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,6 @@
-import { createClient } from '@supabase/supabase-javascript';
+import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = 'https://ianlilyiumkvkowcnuawt.supabase.co';
-// Supabase Dashboard -> Project Settings -> API 에서 복사한 anon public 키를 넣어주세요.
 const SUPABASE_ANON_KEY = 'sb_publishable_n96aYfBdNsSLtQ_wbAJe2g_VVVAcWVW';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
