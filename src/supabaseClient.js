c6749a4329a22cc2c177799224f7a3768bce5e44
Choiseonkyeong <94091135+Choiseonkyeong@users.noreply.github.com>
2026-08-27 14:37:31 +0900

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://ianlilyiumkvkowcnuawt.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_n96aYfBdNsSLtQ_wbAJe2g_VVVAcWVW';
+const supabaseUrl = 'https://lanllylumkvkowcnuawt.supabase.co';
+const supabaseAnonKey = 'sb_publishable_n96aYfBdNsSLtQ_wbAJe2g_VVVAcWVW';
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
