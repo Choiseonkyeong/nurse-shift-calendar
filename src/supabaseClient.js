74be5bc8c2e0ac5ce81825705e2f68b659812a72
Choiseonkyeong <94091135+Choiseonkyeong@users.noreply.github.com>
2026-08-27 16:09:13 +0900

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
-// 프로젝트 ID(seuamhutmdcjyacxuqyt) 반영
+// Choiseonkyeong's Project 대시보드에서 복사한 URL 및 Anon Key
 const supabaseUrl = 'https://seuamhutmdcjyacxuqyt.supabase.co';
-
-// 해당 프로젝트 대시보드(Project Settings > API)에서 복사한 anon public key를 넣어주세요.
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhbmlseWl1bWt2a293Y251YXd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc3OTg5MjIsImV4cCI6MjEwMzM3NDkyMn0.1AZFoSC4_LHUkTo2flmGN7vPYB8MgEICaDohbn3fPg0';
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+
